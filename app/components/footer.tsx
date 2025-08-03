37bec9c577beb8d8f454f7a237a064ab55868f99
samrat <samratshakya5@gmail.com>
2025-08-04 01:25:08 +0545

    update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -37,7 +37,7 @@ function SocialLinks() {
 export default function Footer() {
   return (
     <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
-      <time>© {YEAR}</time>{" "}
+      <time>{`Samrat Shakya`} © {`2021 - ` + YEAR}</time>{" "}
       <a
         className="no-underline"
         href={socialLinks.twitter}

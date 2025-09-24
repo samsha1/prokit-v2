0b1566e96eee408a81d3385ff6a34d04f930939f
samrat <samratshakya5@gmail.com>
2025-09-24 21:37:59 +0545

    footer rss link removed and minor updates

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import {
   FaXTwitter,
   FaGithub,
-  FaRss,
+  // FaRss,
   FaLinkedinIn,
 } from "react-icons/fa6";
 import { TbMailFilled } from "react-icons/tb";
@@ -27,9 +27,9 @@ function SocialLinks() {
       <SocialLink href={socialLinks.github} icon={FaGithub} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
-      <a href="/rss.xml" target="_self">
+      {/* <a href="/rss.xml" target="_self">
         <FaRss />
-      </a>
+      </a> */}
     </div>
   );
 }
